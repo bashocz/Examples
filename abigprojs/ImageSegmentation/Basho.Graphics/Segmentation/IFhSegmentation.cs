8393fd51305526e966988ac4b2b9b149017c3391
bashocz <pbaros@seznam.cz>
2013-02-27 08:38:21 +0100

    abigprojs: removed imageSegmentation project

--- abigprojs/ImageSegmentation/Basho.Graphics/Segmentation/IFhSegmentation.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Basho.Graphics.Segmentation
-{
-    public interface IFhSegmentation
-    {
-        int[,] BuildSegments(int width, int height, byte[,] pixels, int k);
-    }
-}
--- abigprojs/ImageSegmentation/fhs.bat
+++
@@ -1,7 +0,0 @@
-pause
-
-fhsbench\bin\Release\fhsbench.exe ..\pictures\dp_test
-fhsbench\bin\Release\fhsbench.exe ..\pictures\BSDS300\images\test
-fhsbench\bin\Release\fhsbench.exe ..\pictires\BSDS300\images\train
-
-pause
